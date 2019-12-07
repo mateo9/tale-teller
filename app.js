b38bc56ac3d4fdcb1965611857c6ae41e5b4bff1
mateo vukusic <mateo.vukusic@krokodil.hr>
2019-12-07 16:36:28 +0100

    Initialize graphql

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodyParse.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello!')
-});
+app.use('/graphql', graphQlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Testing', 'Testing'];
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName
+    }
+  },
+  graphiql: true
+}))
 
 app.listen(3000);
