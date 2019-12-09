c46e63e254a5b0639f71d056e15176d4dc7ff583
mateo vukusic <mateo.vukusic@krokodil.hr>
2019-12-09 21:58:43 +0100

    Change promise to async

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,17 +1,19 @@
 const User = require('../../models/User');
 
 module.exports =   {
-  createUser: args => {
-    let user = User.create({
-      user_name: args.userInput.user_name,
-      first_name: args.userInput.first_name,
-      last_name: args.userInput.last_name,
-      email: args.userInput.email,
-      password: args.userInput.password,
-      avatar: args.userInput.avatar
-    }).then((user) => {
-      console.log('tu sam');
-      console.log(user);
-    });
+  createUser: async (args) => {
+    try {
+      let user = await User.create({
+        user_name: args.userInput.user_name,
+        first_name: args.userInput.first_name,
+        last_name: args.userInput.last_name,
+        email: args.userInput.email,
+        password: args.userInput.password,
+        avatar: args.userInput.avatar
+      })
+      console.log(user)
+    } catch(err) {
+      console.log(err)
+    }
   }
 }
