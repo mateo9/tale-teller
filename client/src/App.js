51bde86779fa293c362f33ea802083aaaf410802
mateo vukusic <mateo.vukusic@krokodil.hr>
2019-12-14 19:37:55 +0100

    Upgrade package

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+
+import UserRegistration from './pages/UserRegistration'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello Tale Teller</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect from='/' to='/register' exact />
+          <Route path='/register'  component={UserRegistration} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ client/src/pages/UserRegistration.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class UserRegistration extends Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+export default UserRegistration;
