b91272caf8787fa58c6d99c2de735cd5d2ed7e0f
mateo vukusic <mateo.vukusic@krokodil.hr>
2019-12-15 22:21:58 +0100

    Add basic registration form and apply css

--- client/src/pages/UserRegistration.js
+++ client/src/pages/UserRegistration.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
+import '../style/RegistrstionPage.css';
+
 class UserRegistration extends Component {
   render() {
-    return <h1>Hello</h1>
+    return(
+      <form className='registration-form'>
+        <div className='form-field'>
+          <input type='text' className='registration-field' placeholder='Username' />
+        </div>
+        <div className='form-field'>
+          <input type='email' className='registration-field' autocomplete="new-password" placeholder='Email' />
+        </div>
+        <div className='form-field form-field-two-inline'>
+          <input type='password' className='registration-field' autocomplete="new-password" placeholder='Password' />
+          <input type='password' className='registration-field' autocomplete="new-password" placeholder='Confirm Password' />
+        </div>
+        <div className='form-field form-field-btn-div'>
+          <button type='submit' className='main-btn submit-btn'>Register Now</button>
+        </div>
+      </form>
+    )
   }
 }
 
