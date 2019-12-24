51f4b196502457ec362f0c00d957e47d29deef11
mateo vukusic <mateo.vukusic@krokodil.hr>
2019-12-24 14:23:48 +0100

    Bind form filed using react ref

--- client/src/pages/UserRegistration.js
+++ client/src/pages/UserRegistration.js
@@ -3,18 +3,26 @@ import React, { Component } from 'react';
 import '../style/RegistrstionPage.css';
 
 class UserRegistration extends Component {
+  constructor(props) {
+    super(props);
+    this.usernameElement = React.createRef();
+    this.emailElement = React.createRef();
+    this.passwordElement = React.createRef();
+    this.confirmPasswordElement = React.createRef();
+  }
+
   render() {
     return(
-      <form className='registration-form'>
+      <form className='registration-form' onSubmit={this.handleRegistration}>
         <div className='form-field'>
-          <input type='text' className='registration-field' placeholder='Username' />
+          <input type='text' className='registration-field' placeholder='Username' ref={this.usernameElement} />
         </div>
         <div className='form-field'>
-          <input type='email' className='registration-field' autocomplete="new-password" placeholder='Email' />
+          <input type='email' className='registration-field' autoComplete="new-password" placeholder='Email' ref={this.emailElement} />
         </div>
         <div className='form-field form-field-two-inline'>
-          <input type='password' className='registration-field' autocomplete="new-password" placeholder='Password' />
-          <input type='password' className='registration-field' autocomplete="new-password" placeholder='Confirm Password' />
+          <input type='password' className='registration-field' autoComplete="new-password" placeholder='Password' ref={this.passwordElement} />
+          <input type='password' className='registration-field' autoComplete="new-password" placeholder='Confirm Password' ref={this.confirmPasswordElement} />
         </div>
         <div className='form-field form-field-btn-div'>
           <button type='submit' className='main-btn submit-btn'>Register Now</button>
